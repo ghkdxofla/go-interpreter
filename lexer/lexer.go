2618f40b8a1748333c44309fd9db9e6970a44830
Taelim Hwang <ghkdxofla@gmail.com>
2022-01-24 02:31:31 +0900

    add nextToken method

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "go-interpreter/token"
+
 // TODO: ch의 byte를 rune으로 변경하면 유니코드, utf-8 지원 가능함
 type Lexer struct {
 	input        string
@@ -12,6 +14,46 @@ func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case 0:
+		tok = newToken(token.EOF, l.ch)
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	var literal string
+
+	if ch == 0 {
+		literal = ""
+	} else {
+		literal = string(ch)
+	}
+
+	return token.Token{Type: tokenType, Literal: literal}
 }
 
 func (l *Lexer) readChar() {
