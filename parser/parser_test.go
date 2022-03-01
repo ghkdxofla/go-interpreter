0368047f3ad48511d8e2b35ceaa4822a78e28881
Taelim Hwang <ghkdxofla@gmail.com>
2022-03-01 23:26:32 +0900

    feat: add test code for parser

---
+++ parser/parser_test.go
@@ -0,0 +1,74 @@
+package parser
+
+import (
+	"go-interpreter/ast"
+	"go-interpreter/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+		let x = 5;
+		let y = 10;
+		let foobar = 838383;
+	`
+
+	l := lexer.New(input) // Init Lexer
+	p := New(l)           // Init Parser
+
+	program := p.ParseProgram()
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Fatalf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	// Type assertion
+	// Link 1: https://stackoverflow.com/questions/49448302/convert-interface-to-struct
+	// Link 2: https://blog.advenoh.pe.kr/go/%ED%83%80%EC%9E%85-%EB%8B%A8%EC%96%B8-Type-Assertion/
+	// Link 3: https://stackoverflow.com/questions/22693275/what-does-asterisk-struct-notation-mean-in-golang
+	// Left value: Zero value of type assertion
+	// Right value: Boolean if the assertion holds in case of I.(T)
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Error("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
+}
